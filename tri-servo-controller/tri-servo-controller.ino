44198f12821ca23a327c7bdfbd0b116ff27e1ca0
Sam-Nicolai Johnston <sam-nicolai.johnston@ssense.com>
2023-07-03 11:34:08 -0400

    Update servo pins after rebuild

--- tri-servo-controller/tri-servo-controller.ino
+++ tri-servo-controller/tri-servo-controller.ino
@@ -1,9 +1,9 @@
 #include "Servo.h"
 #include "InverseKinematics.h"
 
-Servo servo1 = Servo(11, 10, 4, 611, true);
-Servo servo2 = Servo(6, 9, 2, 2315, false);
-Servo servo3 = Servo(3, 5, 6, 373, true);
+Servo servo1 = Servo(11, 10, 2, 702, false);
+Servo servo2 = Servo(6, 9, 6, 2509, true);
+Servo servo3 = Servo(3, 5, 4, 473, true);
 
 void setup() {
   Serial.begin(115200);
@@ -32,50 +32,58 @@ void loop() {
   float theta3;
 
   int timeInterval = 2000;
+  float zValue = -150.0;  // - 25.0 * sin(millis() / 100.0);
 
-  if (time < timeInterval) {
-    int status = delta_calcInverse(0, 0, -100, theta1, theta2, theta3);
+  // if (time < timeInterval) {
+    int status = delta_calcInverse(0, 0, static_cast<int>(zValue), theta1, theta2, theta3);
 
-    servo1.setPositionInDeg(theta1);
-    servo2.setPositionInDeg(theta2);
-    servo3.setPositionInDeg(theta3);
-  } else if (time < 2 * timeInterval) {
-    int status = delta_calcInverse(0, 0, -150, theta1, theta2, theta3);
+  int pos = servo2.getCurrentPosition();
 
-    servo1.setPositionInDeg(theta1);
-    servo2.setPositionInDeg(theta2);
-    servo3.setPositionInDeg(theta3);
-  } else if (time < 3 * timeInterval) {
-    int status = delta_calcInverse(50, 0, -100, theta1, theta2, theta3);
+  Serial.println(pos);
 
     servo1.setPositionInDeg(theta1);
     servo2.setPositionInDeg(theta2);
     servo3.setPositionInDeg(theta3);
-  } else if (time < 4 * timeInterval) {
-    int status = delta_calcInverse(-50, 0, -100, theta1, theta2, theta3);
+    // servo1.setPositionInDeg(45);
+    // servo2.setPositionInDeg(45);
+    // servo3.setPositionInDeg(0);
+  // } else if (time < 2 * timeInterval) {
+  //   int status = delta_calcInverse(0, 0, -150, theta1, theta2, theta3);
 
-    servo1.setPositionInDeg(theta1);
-    servo2.setPositionInDeg(theta2);
-    servo3.setPositionInDeg(theta3);
-  } else if (time < 5 * timeInterval) {
-    int status = delta_calcInverse(0, 50, -100, theta1, theta2, theta3);
+  //   servo1.setPositionInDeg(theta1);
+  //   servo2.setPositionInDeg(theta2);
+  //   servo3.setPositionInDeg(theta3);
+  // } else if (time < 3 * timeInterval) {
+  //   int status = delta_calcInverse(50, 0, -100, theta1, theta2, theta3);
 
-    servo1.setPositionInDeg(theta1);
-    servo2.setPositionInDeg(theta2);
-    servo3.setPositionInDeg(theta3);
-  } else if (time < 6 * timeInterval) {
-    int status = delta_calcInverse(0, -50, -100, theta1, theta2, theta3);
+  //   servo1.setPositionInDeg(theta1);
+  //   servo2.setPositionInDeg(theta2);
+  //   servo3.setPositionInDeg(theta3);
+  // } else if (time < 4 * timeInterval) {
+  //   int status = delta_calcInverse(-50, 0, -100, theta1, theta2, theta3);
 
-    servo1.setPositionInDeg(theta1);
-    servo2.setPositionInDeg(theta2);
-    servo3.setPositionInDeg(theta3);
-  } else {
-    int status = delta_calcInverse(0, 0, -100, theta1, theta2, theta3);
+  //   servo1.setPositionInDeg(theta1);
+  //   servo2.setPositionInDeg(theta2);
+  //   servo3.setPositionInDeg(theta3);
+  // } else if (time < 5 * timeInterval) {
+  //   int status = delta_calcInverse(0, 50, -100, theta1, theta2, theta3);
 
-    servo1.setPositionInDeg(theta1);
-    servo2.setPositionInDeg(theta2);
-    servo3.setPositionInDeg(theta3);
-  }
+  //   servo1.setPositionInDeg(theta1);
+  //   servo2.setPositionInDeg(theta2);
+  //   servo3.setPositionInDeg(theta3);
+  // } else if (time < 6 * timeInterval) {
+  //   int status = delta_calcInverse(0, -50, -100, theta1, theta2, theta3);
+
+  //   servo1.setPositionInDeg(theta1);
+  //   servo2.setPositionInDeg(theta2);
+  //   servo3.setPositionInDeg(theta3);
+  // } else {
+  //   int status = delta_calcInverse(0, 0, -150, theta1, theta2, theta3);
+
+  //   servo1.setPositionInDeg(theta1);
+  //   servo2.setPositionInDeg(theta2);
+  //   servo3.setPositionInDeg(theta3);
+  // }
 }
 
   // Serial.print(status);
