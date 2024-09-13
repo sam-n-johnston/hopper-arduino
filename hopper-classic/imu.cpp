fd5d738bc4429ea5e3bf1b5e8b2f078197427834
Sam-Nicolai Johnston <sam-nicolai.johnston@ssense.com>
2024-09-13 18:08:47 -0400

    simplify IMU

--- hopper-classic/imu.cpp
+++ hopper-classic/imu.cpp
@@ -5,15 +5,13 @@ IMU::IMU() {}
 void IMU::begin()
 {
     Serial.println("Starting IMU... ");
+    bool test3 = Wire.setSDA(16);
+    bool test4 = Wire.setSCL(17);
     Wire.begin();
     while (!bno08x.begin_I2C(BNO08x_I2CADDR_DEFAULT, &Wire))
     {
         Serial.println("Failed to find BNO08x chip");
         delay(100);
-        // while (1)
-        // {
-        //     delay(10);
-        // }
     }
 
     setReports();
@@ -23,23 +21,10 @@ void IMU::begin()
 
 void IMU::setReports()
 {
-
-    // if (!bno08x.enableReport(SH2_LINEAR_ACCELERATION))
-    // {
-    //     Serial.println("Could not enable linear acceleration");
-    // }
-    // if (!bno08x.enableReport(SH2_GRAVITY))
-    // {
-    //     Serial.println("Could not enable gravity vector");
-    // }
     if (!bno08x.enableReport(SH2_GAME_ROTATION_VECTOR))
     {
         Serial.println("Could not enable rotation vector");
     }
-    // if (!bno08x.enableReport(SH2_GYROSCOPE_CALIBRATED))
-    // {
-    //     Serial.println("Could not enable gyroscope");
-    // }
 }
 
 void IMU::getSensorData()
@@ -59,43 +44,22 @@ void IMU::getSensorData()
 
     switch (sensorValue.sensorId)
     {
-    // case SH2_LINEAR_ACCELERATION:
-    //     lastLinearAcceleration.x = sensorValue.un.linearAcceleration.x;
-    //     lastLinearAcceleration.y = sensorValue.un.linearAcceleration.y;
-    //     lastLinearAcceleration.z = sensorValue.un.linearAcceleration.z;
-
-    //     currentMillis = millis();
-
-    //     timeSinceLastMeasurementInMs =
-    //         currentMillis - this->lastAccelerationMeasurementTimeInMs;
-
-    //     this->lastComputedSpeed.x =
-    //         lastLinearAcceleration.x * timeSinceLastMeasurementInMs;
-    //     this->lastComputedSpeed.y =
-    //         lastLinearAcceleration.y * timeSinceLastMeasurementInMs;
-    //     this->lastComputedSpeed.z =
-    //         lastLinearAcceleration.z * timeSinceLastMeasurementInMs;
-
-    //     this->lastAccelerationMeasurementTimeInMs = currentMillis;
-
-    //     break;
-    // case SH2_GRAVITY:
-    //     lastGravity.x = sensorValue.un.gravity.x;
-    //     lastGravity.y = -sensorValue.un.gravity.y;
-    //     lastGravity.z = -sensorValue.un.gravity.z;
-    //     break;
     case SH2_GAME_ROTATION_VECTOR:
+        // Serial.print("Geo-Magnetic Rotation Vector - r: ");
+        // Serial.print(sensorValue.un.geoMagRotationVector.real);
+        // Serial.print(" i: ");
+        // Serial.print(sensorValue.un.geoMagRotationVector.i);
+        // Serial.print(" j: ");
+        // Serial.print(sensorValue.un.geoMagRotationVector.j);
+        // Serial.print(" k: ");
+        // Serial.println(sensorValue.un.geoMagRotationVector.k);
+        // break;
         lastOrientation = quaternionToEuler(
             sensorValue.un.gameRotationVector.real,
             sensorValue.un.gameRotationVector.i,
             sensorValue.un.gameRotationVector.j,
             sensorValue.un.gameRotationVector.k);
         break;
-    // case SH2_GYROSCOPE_CALIBRATED:
-    //     lastAngularVelocity.x = -sensorValue.un.gyroscope.x;
-    //     lastAngularVelocity.y = sensorValue.un.gyroscope.y;
-    //     lastAngularVelocity.z = sensorValue.un.gyroscope.z;
-    //     break;
     }
 }
 
@@ -133,44 +97,22 @@ Vector IMU::quaternionToEuler(float qr, float qi, float qj, float qk)
     tempVector.z *= RAD_TO_DEG;
 
     // Calibrate with offset
-    tempVector.x += 1.0;
-    tempVector.y -= 4.4;
+    tempVector.x += 0.6;
+    tempVector.y -= 3.5;
 
     // Calibrate with factor
     // tempVector.x = tempVector.x > 0.0 ? tempVector.x * 0.974026 : tempVector.x * 1.023891;
     // tempVector.y = tempVector.y > 0.0 ? tempVector.y * 1.045296 : tempVector.y * 0.952381;
 
-    Serial.print("Rotation - X: ");
-    Serial.print(tempVector.x);
-    Serial.print(",\tY: ");
-    Serial.print(tempVector.y);
-    Serial.println();
+    // Serial.print("Rotation - X: ");
+    // Serial.print(tempVector.x);
+    // Serial.print(",\tY: ");
+    // Serial.print(tempVector.y);
+    // Serial.println();
 
     return tempVector;
 }
 
-// Vector IMU::quaternionToEuler(float qr, float qi, float qj, float qk, bool degrees)
-// {
-//     float sqr = sq(qr);
-//     float sqi = sq(qi);
-//     float sqj = sq(qj);
-//     float sqk = sq(qk);
-
-//     Vector tempVector;
-
-//     tempVector.z = atan2(2.0 * (qi * qj + qk * qr), (sqi - sqj - sqk + sqr));
-//     tempVector.y = asin(-2.0 * (qi * qk - qj * qr) / (sqi + sqj + sqk + sqr));
-//     tempVector.x = atan2(2.0 * (qj * qk + qi * qr), (-sqi - sqj + sqk + sqr));
-
-//     if (degrees)
-//     {
-//         tempVector.x *= RAD_TO_DEG;
-//         tempVector.y *= RAD_TO_DEG;
-//         tempVector.z *= RAD_TO_DEG;
-//     }
-
-//     return tempVector;
-// }
 
 Vector IMU::getLinearAcceleration()
 {
